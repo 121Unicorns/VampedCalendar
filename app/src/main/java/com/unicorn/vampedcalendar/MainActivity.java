e0600eea233bca4a7420a2a2b952dc3dc43ff5e6
121Unicorns <17795210+121Unicorns@users.noreply.github.com>
2021-11-03 20:21:36 +0300

    Fifth Commit

--- app/src/main/java/com/unicorn/vampedcalendar/MainActivity.java
+++ app/src/main/java/com/unicorn/vampedcalendar/MainActivity.java
@@ -3,12 +3,29 @@ package com.unicorn.vampedcalendar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.unicorn.vampedcalendar.listener.OnDateClickedListener;
+import com.unicorn.vampedcalendar.model.Event;
+import com.unicorn.vampedcalendar.model.EventDate;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    VampedCalendar vampedCalendar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        vampedCalendar = findViewById(R.id.vCalendar);
+
+        vampedCalendar.setNoEventDayShape(VampedCalendar.EventShape.DOT);
+        vampedCalendar.setNoEventDayBackgroundColor(getResources().getColor(R.color.colorDefault));
+
+
     }
 }
